735f5e15a8a92674cc75926d0edec3757a25a60f
TrDucChinh <chinhdevptit@gmail.com>
2024-04-07 12:32:33 +0700

    refactor: delete comment code

--- src/main/java/org/s2booker_app/main/Main.java
+++ src/main/java/org/s2booker_app/main/Main.java
@@ -1,6 +1,6 @@
 package org.s2booker_app.main;
 
-import org.s2booker_app.view.MainScene;
+import org.s2booker_app.view.*;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -8,13 +8,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     public void start(Stage primaryStage) {
-//        BookRoomScene userScene = new BookRoomScene();
-//        InputDialogScene inputDialog = new InputDialogScene();
 
-//        Scene scene = new Scene(userScene.bookerScene()); //1300 x 620
-//        Scene dialogScene = new Scene(inputDialog.inputDialog()); //410x 230
         MainScene startScene = new MainScene();
-
         Scene start = new Scene(startScene.startScene());
         // Set the Scene
         primaryStage.setResizable(false);
